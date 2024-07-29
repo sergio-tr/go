53884c8807bf764744a02e578647040aee518788
sergio-tr <truebanosergio@gmail.com>
2024-07-30 01:27:35 +0200

    exercism go exercise 11

---
+++ card-tricks/card_tricks.go
@@ -0,0 +1,54 @@
+package cards
+
+const IndexOutOfRange = -1
+
+// FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
+func FavoriteCards() []int {
+	return []int{2, 6, 9}
+}
+
+// isIndexOutOfRange returns whether the index is out of range for the given slice.
+func isIndexOutOfRange(slice []int, index int) bool {
+	return index < 0 || index >= len(slice)
+}
+
+// GetItem retrieves an item from a slice at given position.
+// If the index is out of range, we want it to return -1.
+func GetItem(slice []int, index int) int {
+
+	if isIndexOutOfRange(slice, index) {
+		return IndexOutOfRange
+	}
+
+	return slice[index]
+}
+
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range the value needs to be appended.
+func SetItem(slice []int, index, value int) []int {
+	if isIndexOutOfRange(slice, index) {
+		return append(slice, value)
+	}
+
+	slice[index] = value
+	return slice
+}
+
+// PrependItems adds an arbitrary number of values at the front of a slice.
+func PrependItems(slice []int, values ...int) []int {
+	return append(values, slice...)
+}
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+	if isIndexOutOfRange(slice, index) {
+		return slice
+	}
+
+	// IMPORTANT:
+	// ---------
+	// if index is 0 : slice[:index] -> []
+	// if (index+1) is len(slice) : slice[index+1:] -> []
+
+	return append(slice[:index], slice[index+1:]...)
+}
